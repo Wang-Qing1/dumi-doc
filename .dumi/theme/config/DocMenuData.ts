a0067eeb8060c78c55cd35b74834bbc5c5a59fd8
wangq <wangqing@nianhuatech.com>
2024-08-08 14:39:22 +0800

    菜单定义规则

--- .dumi/theme/config/DocMenuData.ts
+++ .dumi/theme/config/DocMenuData.ts
@@ -12,14 +12,26 @@ type DocMenuItem = {
  *  菜单顺序用于生成侧边栏菜单 以及 底部的导航栏（上一个与下一个）
  */
 const DocMenuData: DocMenuItem[] = [
-  // 第一级目录
+  /**
+   * 一级菜单，包含/docs/xxx目录下的.md文件（类型为；doc）以及目录文件夹（类型为：menu）
+   * 示例：文件夹名为 xxx 相对路径为 /docs/xxx
+   *  { id: 1, parentId: 0, folder: 'xxx', type: "doc", title: "内容标题（即.md文件标题）", link: ''}
+   *  或者
+   *  { id: 1, parentId: 0, folder: 'xxx', type: "menu", title: "目录名称", link: ''},
+   */
   { id: 1, parentId: 0, folder: 'first', type: "doc", title: "第一层目录文件", link: ''},
   { id: 2, parentId: 0, folder: 'first', type: "doc", title: "第一层文件-1", link: '' },
   { id: 3, parentId: 0, folder: 'first', type: "doc", title: "第一层文件-2", link: '' },
   { id: 4, parentId: 0, folder: 'first', type: "menu", title: '第二层-1', link: '' },
   { id: 5, parentId: 0, folder: 'first', type: "menu", title: '第二层-2', link: '' },
 
-  // 第二级目录
+  /**
+   * 二级菜单，包含/docs/xxx/sss目录下的.md文件（类型为；doc）以及目录文件夹（类型为：menu）
+   * 示例：文件夹名为 sss 相对路径为 /docs/xxx/sss 此处的folder与一级菜单对齐，仍为 xxx
+   *  { id: 101, parentId: 1, folder: 'xxx', type: "doc", title: "内容标题（即.md文件标题）", link: ''}
+   *  或者
+   *  { id: 101, parentId: 1, folder: 'xxx', type: "menu", title: "目录名称", link: ''},
+   */
   { id: 401, parentId: 4, folder: 'first', type: 'doc', title: '第二层目录文件', link: '' },
   { id: 402, parentId: 4, folder: 'first', type: 'doc', title: '第二层文件 1', link: '' },
   { id: 403, parentId: 4, folder: 'first', type: 'doc', title: '第二层文件 2', link: '' },
@@ -29,19 +41,37 @@ const DocMenuData: DocMenuItem[] = [
   { id: 503, parentId: 5, folder: 'first', type: 'doc', title: '第二层第二目录文件 2', link: '' },
   { id: 504, parentId: 5, folder: 'first', type: 'menu', title: '第三层', link: '' },
 
-  // 第三级目录
+  /**
+   * 三级菜单，包含/docs/xxx/sss/aaa目录下的.md文件（类型为；doc）以及目录文件夹（类型为：menu）
+   * 示例：文件夹名为 aaa 相对路径为 /docs/xxx/sss/aaa 此处的folder与一级菜单对齐，仍为 xxx
+   *  { id: 10101, parentId: 101, folder: 'xxx', type: "doc", title: "内容标题（即.md文件标题）", link: ''}
+   *  或者
+   *  { id: 10101, parentId: 101, folder: 'xxx', type: "menu", title: "目录名称", link: ''},
+   */
   { id: 50401, parentId: 504, folder: 'first', type: 'doc', title: '第三层目录文件', link: '' },
   { id: 50402, parentId: 504, folder: 'first', type: 'doc', title: '第三层文件 - 1', link: '' },
   { id: 50403, parentId: 504, folder: 'first', type: 'doc', title: '第三层文件 - 2', link: '' },
   { id: 50404, parentId: 504, folder: 'first', type: 'menu', title: '第四层', link: '' },
 
-  // 第四级目录
+  /**
+   * 四级菜单，包含/docs/xxx/sss/aaa/bbb 目录下的.md文件（类型为；doc）以及目录文件夹（类型为：menu）
+   * 示例：文件夹名为 bbb 相对路径为 /docs/xxx/sss/aaa/bbb 此处的folder与一级菜单对齐，仍为 xxx
+   *  { id: 1010101, parentId: 10101, folder: 'xxx', type: "doc", title: "内容标题（即.md文件标题）", link: ''}
+   *  或者
+   *  { id: 1010101, parentId: 10101, folder: 'xxx', type: "menu", title: "目录名称", link: ''},
+   */
   { id: 5040401, parentId: 50404, folder: 'first', type: 'doc', title: '第四层目录文件', link: '' },
   { id: 5040402, parentId: 50404, folder: 'first', type: 'doc', title: '第四层文件 1', link: '' },
   { id: 5040403, parentId: 50404, folder: 'first', type: 'doc', title: '第四层文件 2', link: '' },
   { id: 5040404, parentId: 50404, folder: 'first', type: 'menu', title: '第五层', link: '' },
 
-  // 第五级目录
+  /**
+   * 五级菜单，包含/docs/xxx/sss/aaa/bbb/ccc 目录下的.md文件（类型为；doc）以及目录文件夹（类型为：menu）
+   * 示例：文件夹名为 ccc 相对路径为 /docs/xxx/sss/aaa/bbb 此处的folder与一级菜单对齐，仍为 xxx
+   *  { id: 101010101, parentId: 1010101, folder: 'xxx', type: "doc", title: "内容标题（即.md文件标题）", link: ''}
+   *  或者
+   *  { id: 101010101, parentId: 1010101, folder: 'xxx', type: "menu", title: "目录名称", link: ''},
+   */
   { id: 504040401, parentId: 5040404, folder: 'first', type: 'doc', title: '第五层目录文件', link: '' },
   { id: 504040402, parentId: 5040404, folder: 'first', type: 'doc', title: '第五层文件 1', link: '' },
   { id: 504040403, parentId: 5040404, folder: 'first', type: 'doc', title: '第五层文件 2', link: '' },
